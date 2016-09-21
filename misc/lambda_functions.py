7f4d77cc6755b21c3683e376e8a4dfc9f8647c31
mathuv <mathuvarman@yahoo.com>
2016-09-21 14:04:35 +1000

    Added explanation to lamda functions and experiment with multiple and optional arguments

--- misc/lambda_functions.py
+++ misc/lambda_functions.py
@@ -1,3 +1,9 @@
+"""
+A lambda function is a function that takes any numbe rof arguments (including optional) and returns value of asingle
+expression. They cant contain commands or more than one expressions.
+"""
+
+
 def f(x):
     return x*2
 
@@ -15,3 +21,16 @@ print g(3)
 print (lambda x: x*2)(3)
 # OUTPUT
 # 6
+
+# multiple arguments
+h = lambda x, y: x*y
+print h(3, 5)
+
+# optional arguments
+i = lambda x, y, z=1: x*y*z
+print i(3, 5)
+print i(3, 5, 5)
+
+# OUTPUT
+# 15
+# 75
