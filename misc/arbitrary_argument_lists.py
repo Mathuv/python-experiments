256ff97158e58f8484e5373c8f2e191639ab50e8
mathuv <mathuvarman@yahoo.com>
2016-09-21 10:15:31 +1000

    Added arbitrary_argument_list.py: usage of * and ** arguments

---
+++ misc/arbitrary_argument_lists.py
@@ -0,0 +1,33 @@
+# use * (*args, *vars) when you are not sure how many arguments might be passed to your function
+def print_everything(*args):
+    for count, thing in enumerate(args):
+        print '{0}.{1}'.format(count, thing)
+
+print_everything('apple', 'banana', 'cabbage')
+
+
+# Use ** to handle names arguments that you have not defined in advance
+def table_things(**kwargs):
+    print('\n')
+    for name, value in kwargs.items():
+        print '{0} = {1}'.format(name, value)
+
+table_things(apple='fruit', cabbage='vegetable')
+
+
+# arbitrary argument lists can be used together with names arguments
+def table_things2(titlestring, **kwargs):
+    print '\n' + titlestring
+    for name, value in kwargs.items():
+        print '{0} = {1}'.format(name, value)
+
+table_things2('Healthy stuffs', apple='fruit', cabbage='vegetable')
+
+
+# * and ** syntax can be used when calling functions
+def print_three_things(a, b, c):
+    print 'a = {0}, b = {1}, c = {2}'.format(a, b, c)
+
+my_list = ['aadvark', 'baboon', 'cat']
+print '\n'
+print_three_things(*my_list)
