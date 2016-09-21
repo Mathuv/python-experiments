764bd190b4afb1ab110c5ba24551a9e64837b218
Mathu Mounasamy <mmounasamy@gmail.com>
2016-09-22 07:58:01 +1000

    Added subplots

---
+++ plotting/subplots.py
@@ -0,0 +1,23 @@
+"""
+The figure() command here is optional because figure(1) will be created by default,
+just as a subplot(111) will be created by default if you don’t manually specify any axes.
+The subplot() command specifies numrows, numcols, fignum where fignum ranges from 1 to numrows*numcols.
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+
+plt.subplot(212)
+plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
+plt.show()
